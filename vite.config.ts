a8befadb75bc1cf5d8947a8a7eb0cab50bb8298b
7thramzy <7thramzy@gmail.com>
2025-07-07 14:12:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -17,11 +18,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      // إضافة ملف index-BQBqcQvy.js كـ external لمنع Vite من معالجته
-      external: ['/dist/assets/index-BQBqcQvy.js'], // تعديل المسار هنا ليعكس المسار الصحيح بعد البناء
-    },
-  },
-  publicDir: path.resolve(__dirname, 'public'),  // تعيين مجلد public إذا كان لديك ملفات ثابتة 
 }));
